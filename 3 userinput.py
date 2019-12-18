75aa9293ea07f1fdb0645b6342f4f7b0b9a0a97c
Vaaiibhav Vaidya <vaaiibhav@live.com>
2019-12-18 08:10:50 +0530

    First Day Python

---
+++ 3 userinput.py
@@ -0,0 +1,4 @@
+a= int(input(" Enter your 1st No"))
+b= int(input("Enter your 2nd No"))
+c= a+b;
+print(c);
---
+++ 4 userStr.py
@@ -0,0 +1,5 @@
+a= input(" Enter your first Name ");
+b= input("Enter your Second Name ");
+c = input(" Enter your Last Name ");
+
+print("Your name is: " , a+" "+b+" "+c);
