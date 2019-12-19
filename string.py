5622b74af7288f6b2e527dbf71a54932b8d2afca
Vaaiibhav Vaidya <vaaiibhav@live.com>
2019-12-19 07:24:59 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,26 @@
+st = "New Name";
+print(st)
+
+st = 24;
+print(st)
+
+st = 14.69;
+print(st);              # This is a float
+
+#  Type Casting 
+
+
+st =int("12");
+vt = int("22");
+print(st + vt)
+
+            # Type Casting Constructs available
+# int("");
+# float("")
+# str("");
+
+
+
+st = 11024;
+print(st)
+print(" asdfjbhasdhfajksdbncv ")
