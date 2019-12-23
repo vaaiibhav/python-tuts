8bced54161f8ee2bf1041373c4f634a31903f7e1
Vaaiibhav Vaidya <vaaiibhav@live.com>
2019-12-23 08:05:36 +0530

    list and tuples

---
+++ sets.py
@@ -0,0 +1,29 @@
+yesethogaya = {"dominor400","CBR350", "Apache310", "KTM390", "NINJA 410"};
+print(yesethogaya);
+print (len(yesethogaya));
+# adding one item
+yesethogaya.add("Suzuki Hayabausa");
+print(yesethogaya);
+print (len(yesethogaya));
+
+# adding a lot of items
+
+yesethogaya.update(["KTM DUKE","Enticer","R15","Avenger","Gixer"])
+print(yesethogaya);
+print (len(yesethogaya));
+
+
+#remove from a set
+
+yesethogaya.remove("Enticer");
+print (yesethogaya);
+
+#Discard from a set
+yesethogaya.discard("Avenger");
+print (yesethogaya);
+
+
+yesethogaya = {"dominor400","CBR350", "Apache310", "KTM390", "NINJA 410"};
+yebhisethogaya={"KTM DUKE","Enticer","R15","Avenger","Gixer","dominor400","CBR350", "Apache310",};
+mai_chodke_sab_set_hogaya = yesethogaya.union(yebhisethogaya);
+print(mai_chodke_sab_set_hogaya);
---
+++ tuples.py
@@ -0,0 +1,6 @@
+JantakaTuple =("Vaibhav",  "CSE", 14, 5.10, 54);
+print(JantakaTuple);
+
+
+print(JantakaTuple[:4]);
+print(len(JantakaTuple))
