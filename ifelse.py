d9831aae47133f07125985a04a7db388258526c7
Vaaiibhav Vaidya <vaaiibhav@live.com>
2019-12-26 07:37:01 +0530

    while loop
    while loop in python tutorial

--- ifelse.py
+++ ifelse.py
@@ -7,3 +7,24 @@ else:
     print('a is not less than b')
 print("value of a",a , "Value of b", b);
         
+# x = 20
+# x == 20
+
+
+userage = int(input("Enter your age  "));
+usergender = (input("Enter your gender  "));
+
+if(usergender =='M'):
+    if(userage >21  ):
+        print("You can marry")
+    else:
+        print("Not allowed to marry")   
+elif(usergender == "F"):
+    if(userage >18):
+        print("allowed to watch the movie");  
+    else:
+        print("Not allowed to marry")       
+
+
+
+
---
+++ whileloop.py
@@ -0,0 +1,35 @@
+# i = 1
+
+# while (i<15):
+#     print(i);
+#     i= i+1;
+#     print("inside while")
+
+# print("Out of while")
+
+
+#Indentation
+
+
+# i= 1;
+
+# while(i<30):
+#     print(i);
+#     if i==15:
+#         break;
+#     i+=1;
+
+
+
+
+i= 45;
+
+while(i<30):
+    print(i);
+    i=+1;
+    i= i+1;
+    i+=1;
+    
+else:
+    print(" is greater than 30");
+
