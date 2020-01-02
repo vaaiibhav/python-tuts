b133d06c067ead8ed9d5e38e92bce4f28e897b3b
Vaaiibhav Vaidya <vaaiibhav@live.com>
2020-01-02 07:23:22 +0530

    Create forloops.py

---
+++ forloops.py
@@ -0,0 +1,19 @@
+# for(x = 0; x<=10; x+2){
+
+
+# }
+
+
+
+hamariList =["Vaibhav",  "CSE", 14, 5.10, 54];
+for li in hamariList:
+    print(li);
+
+
+
+
+for x in range(1,10,2):
+    print(x);
+else:
+    print("Finished Printing")
+
