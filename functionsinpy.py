90f7c1b71f577df944203c791d41ca66efdcc9fa
Vaaiibhav Vaidya <vaaiibhav@live.com>
2020-01-03 07:50:17 +0530

    Create functionsinpy.py

---
+++ functionsinpy.py
@@ -0,0 +1,32 @@
+def kuchtohaddKarna(a=8,b=13):
+    a+b;
+    print(a+b)
+
+print ("Hello World");
+
+
+
+kuchtohaddKarna();
+
+
+
+kuchtohaddKarna(13,);
+
+kuchtohaddKarna(1.054,1346);
+
+kuchtohaddKarna(b=28);
+
+
+# Return Type function
+
+def MaiReturnKarunga(a,b):
+    return a+b;
+
+
+returnkivalue = MaiReturnKarunga(7,21);
+print(returnkivalue);
+
+returnkivalue = MaiReturnKarunga(7,returnkivalue);
+print(returnkivalue);
+
+# 24+32
